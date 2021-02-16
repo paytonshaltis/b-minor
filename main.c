7d975db4478afe63322564631db05fb40a77a824
Payton Shaltis <shaltisp@gmail.com>
2021-02-15 23:25:12 -0500

    29/30 test cases work! Still have to limit sizes, deal with new lines inside of strings, check multi-lined comments, check signed numbers, and review all test cases.

--- main.c
+++ main.c
@@ -1,26 +1,106 @@
 #include "token.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdbool.h>
 
 extern FILE *yyin;
 extern int yylex();
 extern char *yytext;
 
-int main()
+int main(int argc, char* argv[])
 {
-    // FIXME: handle command line switches, e.g. "-scan"
-    //        hint: man getopt_long_only()
 
-    // FIXME: specify filename on command line
-    // FIXME: bminor source code requires filename suffix .bminor
-    yyin = fopen("program.c","r");
-    if(!yyin) {
-        printf("could not open program.c!\n");
-        return 1;
-    }
+    char* filename = "";                    //name of the source file used in command line  
+    int scan = 0;                           //flag that indicates the "-scan" option was used
+    int opt = 0;                            //return value for getopt_long_only()
+    int index = 0;                          //index of the option stored here by getopt_long_only()
+    char* tokenArray[TOKEN_ERROR + 1] = {  //array that maps token value to name (implicitly through index)
+        "EOF",
+        "ARRAY",
+        "BOOLEAN",
+        "CHAR",
+        "ELSE",
+        "FALSE",
+        "FOR",
+        "FUNCTION",
+        "IF",
+        "INTEGER",
+        "PRINT",
+        "RETURN",
+        "STRING",
+        "TRUE",
+        "VOID",
+        "COLON",
+        "SEMICOLON",
+        "COMMA",
+        "LEFT_BRACKET",
+        "RIGHT_BRACKET",
+        "LEFT_CURLY",
+        "RIGHT_CURLY",
+        "LEFT_PAREN",
+        "RIGHT_PAREN",
+        "ASSIGNMENT",
+        "CARROT",
+        "ADD",
+        "SUBTRACT",
+        "INCREMENT",
+        "DECREMENT",
+        "MULTIPLY",
+        "DIVIDE",
+        "MOD",
+        "EQUAL",
+        "GE",
+        "LE",
+        "LESS",
+        "GREATER",
+        "NOT_EQUAL",
+        "IDENTIFIER",
+        "INT_LITERAL",
+        "CHAR_LITERAL",
+        "STRING_LITERAL",
+        "LOGICAL_OR",
+        "LOGICAL_AND"
+        "LOGICAL_NOT"
+        "ERROR"  
+    };    
+    //array of options; currently only contains "scan" and the required "all-0s" option structs
+    struct option options[] = { 
+        {"scan", required_argument, &scan, 1}, 
+        {0, 0, 0, 0} 
+    };
 
-    while(1) {
-        token_t t = yylex();
-        if(t==TOKEN_EOF) break;
-        printf("token: %d  text: %s\n",t,yytext);
+    //while there are more options to consider
+    opt = getopt_long_only(argc, argv, "", options, &index);
+
+    if(scan == 1) {
+        //the argument for the "scan" option is the filename
+        filename = optarg;
+        
+        //tries to open the source file
+        yyin = fopen(filename,"r");
+        if(!yyin) {
+            printf("could not open program.c!\n");
+            return 1;
+        }
+
+        //loops until end of file (TOKEN_EOF) or invalid token (TOKEN_ERROR)
+        while(1) {
+            token_t t = yylex();
+            if(t==TOKEN_EOF) {
+                exit(0);
+            }
+            else if(t==TOKEN_ERROR) {
+                fprintf(stderr, "scan error: %s is not a valid token.\n", yytext);
+                exit(1);
+            }
+            else if(t==TOKEN_IDENT || t==TOKEN_INTLIT || t==TOKEN_CHARLIT || t==TOKEN_STRINGLIT) {
+                printf("%s %s\n",tokenArray[t], yytext);
+            }
+            else {
+                printf("%s\n",tokenArray[t]);
+            }
+        }
     }
 }
--- scanner.flex
+++ scanner.flex
@@ -1,14 +1,76 @@
 %{
 #include "token.h"
+int mylineno = 0;
 %}
 DIGIT  [0-9]
 LETTER [a-zA-Z]
 %%
-(" "|\t|\n)  /* skip whitespace */
- \+        { return TOKEN_ADD; }
-while      { return TOKEN_WHILE; }
-{LETTER}+  { return TOKEN_IDENT; }
-{DIGIT}+   { return TOKEN_NUMBER; }
-.          { return TOKEN_ERROR; }
+(" "|\t|\n|\r) /* skip whitespace */
+"//".*      /* skips single line C++ style comments */
+array       { return TOKEN_ARRAY; }
+boolean     { return TOKEN_BOOLEAN; }
+char        { return TOKEN_CHAR; }
+else        { return TOKEN_ELSE; }
+false       { return TOKEN_FALSE; }
+for         { return TOKEN_FOR; }
+function    { return TOKEN_FUNCTION; }
+if          { return TOKEN_IF; }
+integer     { return TOKEN_INTEGER; }
+print       { return TOKEN_PRINT; }
+return      { return TOKEN_RETURN; }
+string      { return TOKEN_STRING; }
+true        { return TOKEN_TRUE; }
+void        { return TOKEN_VOID; }
+:           { return TOKEN_COLON; }
+;           { return TOKEN_SEMICOLON; }
+,           { return TOKEN_COMMA; }
+\[          { return TOKEN_LBRACKET; }
+\]          { return TOKEN_RBRACKET; }
+\{          { return TOKEN_LCURLY; }
+\}          { return TOKEN_RCURLY; }
+\(          { return TOKEN_LPAREN; }
+\)          { return TOKEN_RPAREN; }
+=           { return TOKEN_ASSIGN; }
+\^          { return TOKEN_CARROT; }
+\+          { return TOKEN_ADD; }
+-           { return TOKEN_SUBTRACT; }
+\++         { return TOKEN_INCREMENT; }
+--          { return TOKEN_DECREMENT; }
+\*          { return TOKEN_MULTIPLY; }
+\/          { return TOKEN_DIVIDE; }
+%           { return TOKEN_MOD; }
+==          { return TOKEN_EQUAL; }
+\>=         { return TOKEN_GE; }
+\<=         { return TOKEN_LE; }
+\<          { return TOKEN_LESS; }
+\>          { return TOKEN_GREATER; }
+!=          { return TOKEN_NEQUAL; }
+({LETTER}|"_")({LETTER}|"_"|{DIGIT})*   { return TOKEN_IDENT; }
+("+"|"-")?{DIGIT}+    { return TOKEN_INTLIT; }
+'(\\?[^\\])'   { return TOKEN_CHARLIT; }
+\"(\\.|[^\\"\n])*\"  { return TOKEN_STRINGLIT; }
+\|\|        { return TOKEN_OR; }
+&&          { return TOKEN_AND; }
+!           { return TOKEN_NOT; }
+"/*"        {
+            int c;
+            printf("Value of C: %i\n", c);
+            while((c = input()) != 0)
+                {
+                if(c == '\n')
+                    ++mylineno;
+                else if(c == '*')
+                    {
+                    if((c = input()) == '/')
+                        break;
+                    else
+                        unput(c);
+                    }
+                }
+                return TOKEN_ERROR;
+                escape: {};
+            }
+
+.           { return TOKEN_ERROR; }
 %%
 int yywrap() { return 1; }
