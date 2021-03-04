e648a3516cf28f3102d3f560122ab1659f9c277c
Payton Shaltis <shaltisp@gmail.com>
2021-03-04 16:38:03 -0500

    updated 'main.c' further, including a function that deals with modifying 'yytext'

--- main.c
+++ main.c
@@ -10,13 +10,56 @@ extern FILE *yyin;
 extern int yylex();
 extern char *yytext;
 
+/* function used to modify yytext for char and string literals */
+void modifyText(token_t t) {
+    
+    /* creates a new pointer for storing characters one by one */
+    int stringsize = strlen(yytext) + 1;
+    char newyytext[stringsize];
+    int textPos = 0;
+
+    /* copies one character at a time, ignoring quotes 
+    and taking proper action for escape characters */
+    for(int i = 0; i < stringsize; i++) {
+        
+        /* in case of starting and ending quotes */
+        if((yytext[i] == '\"' && t == TOKEN_STRINGLIT) || (yytext[i] == '\'' && t == TOKEN_CHARLIT)) {
+            /* nothing should be duplicated */
+        }
+        
+        /* in case of escape sequence */
+        else if(yytext[i] == '\\') {
+            if(yytext[i+1] == 'n') {
+                newyytext[textPos] = 10;
+                textPos++;
+                i++;
+            }
+            else if(yytext[i+1] == '0') {
+                newyytext[textPos] = 0;
+                textPos++;
+                i++;
+            }
+            else {
+                newyytext[textPos] = yytext[i + 1];
+                textPos++;
+                i++;
+            }
+        }
+        
+        /* in case of any other character */
+        else {
+            newyytext[textPos] = yytext[i];
+            textPos++;
+        }
+    }
+    
+    /* yytext then points to the modified string, can be printed */
+    yytext = newyytext;
+}
+
+/* main function */
 int main(int argc, char* argv[]) {
 
-    int scan = 0;                           /* flag that indicates the "-scan" option was used */
-    int opt = 0;                            /* return value for getopt_long_only() */
-    int index = 0;                          /* index of the option stored here by getopt_long_only() */
-    int stringsize;                         /* stores the size of 'yytext', used when printing string and char literals */
-    char* filename = "";                    /* name of the source file used in command line */
     char* tokenArray[TOKEN_ERROR + 1] = {   /* array that maps token value to name (implicitly through index) */
         "EOF",
         "ARRAY",
@@ -68,81 +111,57 @@ int main(int argc, char* argv[]) {
     };    
     
     /* array of options; currently only contains "scan" and the required "all-0s" option structs */
+    int scanFlag = 0;                   
+    int index = 0;
     struct option options[] = { 
-        {"scan", required_argument, &scan, 1}, 
+        {"scan", required_argument, &scanFlag, 1}, 
         {0, 0, 0, 0} 
     };
 
     /* gets options from the command line */
-    opt = getopt_long_only(argc, argv, "", options, &index);
+    int opt = getopt_long_only(argc, argv, "", options, &index);
 
-    if(scan == 1) {
-        /* the argument for the "scan" option is the filename */
-        filename = optarg;
-        
-        /* tries to open the source file */
-        yyin = fopen(filename,"r");
-        if(!yyin) {
-            fprintf(stderr, "Error, could not open %s!\n",filename);
-            exit(1);
-        }
+    /* the argument for each option is the filename */
+    char* filename = optarg;
+    
+    /* tries to open the source file */
+    yyin = fopen(filename,"r");
+    if(!yyin) {
+        fprintf(stderr, "Error, could not open %s!\n",filename);
+        exit(1);
+    }
+    if(strlen(filename) < 8 || strcmp(filename + strlen(filename) - 7, ".bminor") != 0) {
+        fprintf(stderr, "Error, %s is not a .bminor source file!\n", filename);
+        exit(1);
+    }
+
+    /* when the '-scan' command line option is used */
+    if(scanFlag == 1) {
 
         /* loops until end of file (TOKEN_EOF) or invalid token (TOKEN_ERROR) */
         while(1) {
             token_t t = yylex();
+            
+            /* reached end of file */
             if(t==TOKEN_EOF) {
                 exit(0);
             }
+            /* reached unrecognized token */
             else if(t==TOKEN_ERROR) {
                 fprintf(stderr, "Scan error: %s is not a valid token.\n", yytext);
                 exit(1);
             }
+            /* reached identifier or an integer literal token */
             else if(t==TOKEN_IDENT || t==TOKEN_INTLIT) {
                 printf("%s %s\n",tokenArray[t], yytext);
             }
+            /* reached char literal or string literal token */
             else if(t==TOKEN_CHARLIT || t==TOKEN_STRINGLIT) {
-                /* stores the size of the yytext c-string in stringsize */
-                stringsize = strlen(yytext) + 1;
-                char newyytext[stringsize];
-                int textPos = 0;
-
-                /* prints one character at a time, ignoring quotes 
-                and taking proper action for escape characters */
-                printf("%s ", tokenArray[t]);
-                for(int i = 0; i < stringsize; i++) {
-                    /* in case of quotes */
-                    if((yytext[i] == '\"' && t == TOKEN_STRINGLIT) || (yytext[i] == '\'' && t == TOKEN_CHARLIT)) {
-                        /* nothing should be duplicated */
-                    }
-                    /* in case of escape sequence */
-                    else if(yytext[i] == '\\') {
-                        if(yytext[i+1] == 'n') {
-                            newyytext[textPos] = 10;
-                            textPos++;
-                            i++;
-                        }
-                        else if(yytext[i+1] == '0') {
-                            newyytext[textPos] = 0;
-                            textPos++;
-                        }
-                        else {
-                            newyytext[textPos] = yytext[i + 1];
-                            textPos++;
-                            i++;
-                        }
-                    }
-                    /* in case of any other character */
-                    else {
-                        newyytext[textPos] = yytext[i];
-                        textPos++;
-                    }
-                }
-                yytext = newyytext;
-                /* prints the new modified string that yytext points to */
-                printf("%s", yytext);
-                printf("\n");
+                /* modifyText() removes quotes and deals with escape characters  */
+                modifyText(t);
+                printf("%s %s\n", tokenArray[t], yytext);
             }
-            /* any token other than EOF, ERROR, IDENT, INTLIT, CHARLIT, or STRINGLIT */
+            /* reached any other token */
             else {
                 printf("%s\n",tokenArray[t]);
             }
