765ac005a8a9319581709690859db5f97f3cfc9f
Payton Shaltis <shaltisp@gmail.com>
2021-04-11 23:31:37 -0400

    updated 'scope.c' with the 'scope_exit()' function implementation.

--- scope.c
+++ scope.c
@@ -13,7 +13,7 @@ void scope_enter() {
     // allocates memory for the new stack node 
     struct stack* newStackNode = malloc(sizeof(*newStackNode));
     
-    // assigns a symbol type based on the scope
+    // assigns a scope symbol based on the scope
     if(theStackTop == NULL) {
         newStackNode->thisScope = SYMBOL_GLOBAL;
     }
@@ -31,4 +31,28 @@ void scope_enter() {
     theStackTop = newStackNode;
 
     return;
+}
+
+// pops the top node from the stack, deleting the hash table associated with it in the process
+void scope_exit() {
+
+    // create a pointer to point to the top node of the stack
+    struct stack* toBeDeleted = theStackTop;
+
+    // if there is more than one node in the stack before exiting scope
+    if(theStackTop->next != NULL) {
+        // the second node becomes the top of the stack
+        theStackTop = theStackTop->next;
+    }
+    // if there is only one node in the stack before exiting scope
+    else {
+        // the top of the stack should point to NULL
+        theStackTop = NULL;
+    }
+
+    // deletes the hash table of the node to be deleted
+    hash_table_delete(toBeDeleted->table);
+
+    // frees the memory allocated to the node being deleted
+    free(toBeDeleted);
 }
