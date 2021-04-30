88608335cba9b58a0fab72b9f14285526e53d9a8
Payton Shaltis <shaltisp@gmail.com>
2021-04-29 20:39:24 -0400

    moving more files over to the ARM VM

--- decl.c
+++ decl.c
@@ -398,7 +398,7 @@ void decl_codegen(struct decl* d) {
             
             // in case of a global string
             if(d->symbol->kind == SYMBOL_GLOBAL) {
-                printf(".data\n\t.global %s\n%s:\t.word ", d->name, d->name);
+                printf(".data\n\t.global %s\n%s:\t.ascii ", d->name, d->name);
                 if(d->value) expr_print(d->value);
                 printf("\n");
             }
